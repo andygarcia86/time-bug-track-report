1e2858e1d9248aaa859e1245d55c0284a43a20ea
Andy Garcia <andygarcia86@gmail.com>
2020-05-08 21:48:59 -0500

    Entity Framework Server side

---
+++ server/Web.Api.Infrastructure/Data/Entities/Project.cs
@@ -0,0 +1,8 @@
+﻿namespace Web.Api.Infrastructure.Data.Entities
+{
+    class Project
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
--- server/Web.Api/Startup.cs
+++ server/Web.Api/Startup.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Web.Api.Infrastructure;
+using Web.Api.Infrastructure.Helpers;
 
 namespace Web.Api
 {
@@ -24,6 +21,11 @@ namespace Web.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            // Data Database configuration
+            var dataConnectionString = Configuration.GetConnectionString(AppConstants.DataConnectionStringName);
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(dataConnectionString));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
