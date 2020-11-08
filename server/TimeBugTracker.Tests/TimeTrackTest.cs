09b3e3421145195967049b82e3f81db30f09f5d2
Andy Garcia <andygarcia86@gmail.com>
2020-11-08 10:24:39 -0500

    Add unit test project

---
+++ server/TimeBugTracker.Tests/TimeTrackTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace TimeBugTracker.Tests
+{
+    public class TimeTrackTest
+    {
+        [Fact]
+        public void GetTimeTrackTest()
+        {
+
+        }
+    }
+}
