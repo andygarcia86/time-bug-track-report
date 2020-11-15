0dba9e6eb400a4cf93c2e343ba0213a0d0452ec8
Andy Garcia <andygarcia86@gmail.com>
2020-11-15 10:47:48 -0500

    Update folder structure

---
+++ server/TimeBugTracker.Tests/ProjectsTest.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace TimeBugTracker.Tests
+{
+    public class ProjectsTest
+    {
+        [Fact]
+        public void GetTimeTrackTest()
+        {
+            Assert.True(5 == 5);
+            Assert.True('A' == 'A');
+        }
+    }
+}
