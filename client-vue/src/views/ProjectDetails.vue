b0b30129718738020c874f5b02a954c9bff4c621
Andy Garcia <andygarcia86@gmail.com>
2020-11-19 09:01:01 -0500

    Add Project Details view

--- client-vue/src/views/ProjectDetails.vue
+++ client-vue/src/views/ProjectDetails.vue
@@ -1,13 +1,22 @@
 <template>
   <div class="project-details">
     <h1>This is the projects details page</h1>
-    
+    <div>
+        {{ project.name }}
+    </div>
   </div>
 </template>
 
 <script>
+import { projects } from '../assets/data/projects-fake'
+
 export default{
   name: 'ProjectDetailsPage',
+  data(){
+    return {
+      project: projects.find((p) => p.id === this.$route.params.id)
+    };
+  }
 
 };
 </script>
