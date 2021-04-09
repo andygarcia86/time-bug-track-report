ea8eb2ec675a832d170abc64a3e76cb78130b9e3
Andy Garcia <andygarcia86@gmail.com>
2021-04-08 21:01:53 -0500

    Add first micro service

--- server/README.md
+++
@@ -1,9 +0,0 @@
-# Time Bug tracking and Report. Server API
-
-.Net Core API Server.
-
-## Requirements
-
-1. .Net Core 2.1
-
-## Tests
--- server/TimeBugTracker.Domain/AppConsts.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace TimeBugTracker.Domain
-{
-    public class AppConsts
-    {
-        public const string DataConnectionStringName = "DataDbConnection";
-    }
-}
