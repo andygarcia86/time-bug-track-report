a0af6baeeb9bdffb4d9a79fd606ac2ed6d8a1065
Andy Garcia <andygarcia86@gmail.com>
2021-04-08 21:10:36 -0500

    Add swager to setup class

--- microservice-timetrack/AraneaTimeTrack/Startup.cs
+++ microservice-timetrack/AraneaTimeTrack/Startup.cs
@@ -25,11 +25,26 @@ namespace AraneaTimeTrack
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            // Register the Swagger generator, defining one or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Aranea TimeTrack Web API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "BL Communications TS API V1");
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
